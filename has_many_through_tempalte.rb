512c851706a0790204df7e28284d3df7cc96b4f2
willnet <netwillnet@gmail.com>
2017-10-02 14:47:58 +0900

    Add missing options

--- has_many_through_tempalte.rb
+++ has_many_through_tempalte.rb
@@ -4,9 +4,9 @@ generate(:model, 'bookmark user:references post:references')
 
 rake('db:migrate')
 
-insert_into_file 'app/models/user.rb', "has_many :bookmarked_posts, class_name: 'Post', through: :bookmark\n", after: "class User < ApplicationRecord\n"
+insert_into_file 'app/models/user.rb', "has_many :bookmarked_posts, class_name: 'Post', source: :post, through: :bookmarks\n", after: "class User < ApplicationRecord\n"
 insert_into_file 'app/models/user.rb', "has_many :bookmarks\n", after: "class User < ApplicationRecord\n"
-insert_into_file 'app/models/post.rb', "has_many :bookmarked_users, class_name: 'User', through: :bookmark\n", after: "class Post < ApplicationRecord\n"
+insert_into_file 'app/models/post.rb', "has_many :bookmarked_users, class_name: 'User', source: :user, through: :bookmarks\n", after: "class Post < ApplicationRecord\n"
 insert_into_file 'app/models/post.rb', "has_many :bookmarks\n", after: "class Post < ApplicationRecord\n"
 
 create_file 'db/seeds.rb' do
