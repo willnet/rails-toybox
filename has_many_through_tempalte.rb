30cf322834fff784e10e1ecde771250db8ddff39
willnet <netwillnet@gmail.com>
2017-10-02 14:43:51 +0900

    Add new template for has many through

---
+++ has_many_through_tempalte.rb
@@ -0,0 +1,20 @@
+generate(:model, 'user')
+generate(:model, 'post')
+generate(:model, 'bookmark user:references post:references')
+
+rake('db:migrate')
+
+insert_into_file 'app/models/user.rb', "has_many :bookmarked_posts, class_name: 'Post', through: :bookmark\n", after: "class User < ApplicationRecord\n"
+insert_into_file 'app/models/user.rb', "has_many :bookmarks\n", after: "class User < ApplicationRecord\n"
+insert_into_file 'app/models/post.rb', "has_many :bookmarked_users, class_name: 'User', through: :bookmark\n", after: "class Post < ApplicationRecord\n"
+insert_into_file 'app/models/post.rb', "has_many :bookmarks\n", after: "class Post < ApplicationRecord\n"
+
+create_file 'db/seeds.rb' do
+  <<~SEED
+    User.create!
+    Post.create!
+    Post.create!
+  SEED
+end
+
+rake('db:seed')
